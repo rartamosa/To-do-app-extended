ee14bfe6f0dd200809e3e3036e16b144db79f5d5
Marta Rosa <marta.r.93@gmail.com>
2022-11-16 15:53:08 +0100

    Added animation and transition for opening and closing mobile menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,15 +6,18 @@ const Navigation = ({ tasksTotal, isMobileMenuOpen, onMobileMenuClose }) => {
   return (
     <>
       <nav
-        className="navigation__collection"
+        className="navigation__collection navigation__overlay_closed"
         style={{ left: isMobileMenuOpen ? "0" : "-75%" }}
       >
         <NavigationItem tasksTotal={tasksTotal} />
       </nav>
       <div
         onClick={onMobileMenuClose}
-        className="navigation__overlay"
-        style={{ left: isMobileMenuOpen ? "0" : "-100%" }}
+        // className="navigation__overlay"
+        // style={{ left: isMobileMenuOpen ? "0" : "-100%" }}
+        className={`"navigation__overlay" ${
+          !isMobileMenuOpen && "navigation__overlay_closed"
+        }`}
       ></div>
     </>
   );
