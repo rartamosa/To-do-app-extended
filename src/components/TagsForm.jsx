460f49f5588e72c91933af57c003b7625d640b57
Marta Rosa <marta.r.93@gmail.com>
2022-11-30 17:51:34 +0100

    Fixed position issue is color picker

--- src/components/TagsForm.jsx
+++ src/components/TagsForm.jsx
@@ -26,53 +26,52 @@ const TagsForm = ({ mode, onModalClose, tagToEdit, handleTagFormSubmit }) => {
 
   return (
     <div className="add-window__overlay">
-      <div
-        className="add-window__container"
-        onClick={() => setIsColorPickerOpen(false)}
-      >
-        <form onSubmit={onTagFormSubmit}>
-          <div className="tags-input__controls-container">
-            <div>
-              <h4 className="add-window__container-controls_titles">tag</h4>
-              <input
-                className="add-window__container_input secondary-main__input users-form__input"
-                type="text"
-                placeholder="Type tag"
-                value={tagName}
-                onChange={(event) => setTagName(event.target.value)}
-              />
+      <div className="add-window__container">
+        <div onClick={() => setIsColorPickerOpen(false)}>
+          <form onSubmit={onTagFormSubmit}>
+            <div className="tags-input__controls-container">
+              <div>
+                <h4 className="add-window__container-controls_titles">tag</h4>
+                <input
+                  className="add-window__container_input secondary-main__input users-form__input"
+                  type="text"
+                  placeholder="Type tag"
+                  value={tagName}
+                  onChange={(event) => setTagName(event.target.value)}
+                />
+              </div>
+              <div className="tags-input__colors-container">
+                <h4 className="add-window__container-controls_titles">Color</h4>
+                <div
+                  className="users-form__img-controls_preview"
+                  style={{ backgroundColor: color }}
+                  onClick={handleColorPickerOpen}
+                ></div>
+              </div>
             </div>
-            <div className="tags-input__colors-container">
-              <h4 className="add-window__container-controls_titles">Color</h4>
-              <div
-                className="users-form__img-controls_preview"
-                style={{ backgroundColor: color }}
-                onClick={handleColorPickerOpen}
-              ></div>
-            </div>
-          </div>
 
-          <div className="add-window__container_buttons">
-            <button
-              onClick={onModalClose}
-              type="button"
-              className="add-window__container_button"
-            >
-              Cancel
-            </button>
-            <button type="submit" className="add-window__container_button">
-              Save
-            </button>
-          </div>
-        </form>
+            <div className="add-window__container_buttons">
+              <button
+                onClick={onModalClose}
+                type="button"
+                className="add-window__container_button"
+              >
+                Cancel
+              </button>
+              <button type="submit" className="add-window__container_button">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+        {isColorPickerOpen && (
+          <TwitterPicker
+            onChange={handleColorChange}
+            //   triangle="top-right"
+            width="204px"
+          />
+        )}
       </div>
-      {isColorPickerOpen && (
-        <TwitterPicker
-          onChange={handleColorChange}
-          //   triangle="top-right"
-          width="204px"
-        />
-      )}
     </div>
   );
 };
